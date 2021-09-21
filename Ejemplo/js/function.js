53e38d181aa297ae3d9c4437c3fa05e4d75a1882
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-09-21 17:04:23 +0200

    21_09_2021_1704
    Creación del proyecto y practicar con los archivos "function.js"

---
+++ Ejemplo/js/function.js
@@ -0,0 +1,38 @@
+// Mi primer proyecto JS
+
+/* 
+Autor: Daniel Moreno
+Version 1.0
+ */
+
+
+// Input
+
+var valorA = 45;
+let valorB = 15;
+const PI = 3.1416;
+var suma;
+
+// Proceso y 
+
+suma = valorA + valorB
+let resta = valorA - valorB
+let multip = valorB*8;
+let potencia = valorA**2;
+let div = valorA / valorB;
+let mod = valorB % valorA; 
+
+
+// Output
+
+if(suma <= 60){
+    console.log("El resultado es menos de 60");
+}
+
+console.log(suma);
+
+for (let i = 0; i < 10; i++) {
+    let tabla = 1 + " x 2 =" + i*2;
+    console.log (tabla);
+    
+}
---
+++ Ejercicio1/js/function.js
@@ -0,0 +1,44 @@
+// Mi primer proyecto JS
+
+/* 
+Autor: Daniel Moreno
+Version 1.0
+ */
+
+
+// Input
+
+let valorA = 10;
+let valorB = 5;
+var valorC = valorA + valorB;
+let valorD
+var name;
+name = prompt("Introduce su nombre");
+alert("Bienvenido "+ name);
+const pi = 3.1416;
+var a,r;
+r = parseFloat(prompt("Ingresar el radio"));
+a = pi*r*r;
+alert("El área es "+a);
+
+let num = prompt("Introduce un numero");
+let text = "";
+const NUP = 2;
+
+
+
+
+// Proceso y cálculo
+
+if (num % NUP ==0) {
+    text = "El numero es par";
+    }else {
+        text = "No es un numero par";}
+    console.log(text);
+    
+
+
+
+
+// Output
+
