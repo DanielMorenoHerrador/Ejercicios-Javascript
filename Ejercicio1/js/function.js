461aefcab85310117243cd9b1561b6f7b01a2ee5
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-09-22 13:15:25 +0200

    22_09_2021_1315h
    Cambio en el idioma y aplicar el ejercicio del IVA

--- Ejercicio1/js/function.js
+++ Ejercicio1/js/function.js
@@ -8,6 +8,7 @@ Version 1.0
 
 // Input
 
+
 let valorA = 10;
 let valorB = 5;
 var valorC = valorA + valorB;
@@ -15,27 +16,36 @@ let valorD
 var name;
 name = prompt("Introduce su nombre");
 alert("Bienvenido "+ name);
+
+
 const pi = 3.1416;
 var a,r;
 r = parseFloat(prompt("Ingresar el radio"));
 a = pi*r*r;
 alert("El área es "+a);
 
-let num = prompt("Introduce un numero");
+let numb
 let text = "";
 const NUP = 2;
 
 
+const IVA = 0.21;
+var num;
+num = parseFloat(prompt("Introduce el precio"));
+result = num + (IVA*num);
+alert("El precio es "+result);
+
+const Numer = 1
 
 
 // Proceso y cálculo
 
-if (num % NUP ==0) {
+if (numb % NUP ==0) {
     text = "El numero es par";
     }else {
         text = "No es un numero par";}
     console.log(text);
-    
+
 
 
 
